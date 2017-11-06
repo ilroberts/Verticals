a265c485f9ca1d57ff3bcdf5cdf1a95f7b32623c
iwan roberts <ilroberts@gmail.com>
2017-11-06 16:50:16 +0000

    added hsqldb connection

--- src/main/java/com/ilroberts/MainVerticle.java
+++ src/main/java/com/ilroberts/MainVerticle.java
@@ -2,15 +2,70 @@ package com.ilroberts;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
-    Logger logger = LoggerFactory.getLogger(MainVerticle.class);
-    public void start(Future<Void> startFuture) throws Exception {
+    private Logger logger = LoggerFactory.getLogger(MainVerticle.class);
 
+    private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+    private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+    private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+    private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+    private static final String SQL_ALL_PAGES = "select Name from Pages";
+    private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+    private JDBCClient dbClient;
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
         logger.info("starting main verticle");
-        startFuture.complete();
+        Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+        steps.setHandler(ar -> {
+            if (ar.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(ar.cause());
+            }
+        });
+    }
+
+    private Future<Void> prepareDatabase() {
+        logger.info("preparing database");
+        Future<Void> future = Future.future();
+
+        dbClient = JDBCClient.createShared(vertx, new JsonObject()
+        .put("url", "jdbc:hsqldb:file:db/wiki")
+        .put("driver_class", "org.hsqldb.jdbcDriver")
+        .put("max_pool_size", 30));
+
+        dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                logger.error("could not open a database connection", ar.cause());
+                future.fail(ar.cause());
+            } else {
+                SQLConnection connection = ar.result();
+                connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+                    connection.close();
+                    if(create.failed()) {
+                        logger.error("database preparation error", create.cause());
+                        future.fail(create.cause());
+                    } else {
+                        future.complete();
+                    }
+                });
+            }
+        });
+        return future;
+    }
+
+    private Future<Void> startHttpServer() {
+        logger.info("starting http server");
+        Future<Void> future = Future.future();
+        return future;
     }
 }
